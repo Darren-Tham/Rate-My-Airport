11a00900e8a924bc595871c0ab964f6488981373
owen <osn53387@uga.edu>
2023-02-03 22:10:46 -0500

    Created New Folders, started on Modals

---
+++ src/components/AirportModal/AirportModal.tsx
@@ -0,0 +1,9 @@
+export default function AirportModal() {
+    return (
+        <div>
+            <p>Value (as a decimal)</p>
+            <p>Airport Acro.</p>
+            <p>Airport Name</p>
+        </div>
+    );
+}
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,3 +1,6 @@
+import "Map.css";
+
+
 export default function Map() {
     return (
         <img src="Rate-My-Airport/src/assets/Blank_US_Map_(states_only).svg" alt="Map of the United States"/>
