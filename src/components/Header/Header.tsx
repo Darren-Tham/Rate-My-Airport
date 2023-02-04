63e3bd1b6c443ef9d689d785a34e2d2ba171b947
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-02-04 12:09:32 -0500

    Fixed header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import "./Header.css";
 export default function Header() {
   return (
     <header>
-      <h1>Rate My Airport</h1>
-      <div className="box"></div>
+      <div className="header-container">
+        <h1>Rate My Airport</h1>
+        <div className="box"></div>
+      </div>
     </header>
   );
 }
