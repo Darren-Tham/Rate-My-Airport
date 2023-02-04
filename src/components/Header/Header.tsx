25292695de8d1588fa51e00a813d01f96e219fee
Darren Tham <darrent9859@gmail.com>
2023-02-04 17:04:13 -0500

    Update dropdown in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,29 @@
+import { useState, useEffect, useRef } from 'react'
 import data from '../../assets/airports.json'
 import "./Header.css";
 import wave from "../../assets/wave.svg";
 
-// const AIRPORTS = data.map()
+const AIRPORTS = data.map(({ NAME }) => NAME)
 
 export default function Header() {
+  const [userInput, setUserInput] = useState("")
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function getDropDownTop() {
+    const marginBottom = 10
+    const inputElem = inputRef.current
+    if (inputElem === null) throw 'inputElem is null'
+    const { offsetHeight, offsetTop } = inputElem
+    return offsetHeight + offsetTop + marginBottom
+  }
+
+  function renderDropDown() {
+    return (
+      <div className="header-input-dropdown" style={{ top: `${getDropDownTop()}px` }}>
+        Hello 
+      </div>
+    ) 
+  }
 
   return (
     <header>
@@ -15,8 +34,14 @@ export default function Header() {
         <div className="header-content">
           <h1>Rate My Airport</h1>
           <span className="header-command">Find an <b>Airport</b></span>
-          <input className="header-input" type="text" placeholder="Airport Name"/>
-
+          <input
+            className="header-input"
+            type="text"
+            placeholder="Airport Name"
+            ref={inputRef}
+            onChange={e => setUserInput(e.target.value)}
+          />
+          {userInput.length > 0 && renderDropDown()}
         </div>
       </div>
     </header>
