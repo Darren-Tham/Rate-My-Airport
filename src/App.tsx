c8b7f5eeb7d33025844e8a32970411e6eb57995a
Darren Tham <darrent9859@gmail.com>
2023-02-03 22:18:55 -0500

    Update Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from './components/Header'
+import Header from './components/Header/Header'
 import './App.css'
 
 export default function App() {
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div>
+            <h1>Rate My Airport</h1>
+            <div className="box"></div>
+        </div>
+    )
+}
