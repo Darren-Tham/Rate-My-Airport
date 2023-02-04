a50052f913499eb1c0d020b87f55a25ca8f31e1c
owen <osn53387@uga.edu>
2023-02-04 02:18:49 -0500

    Established Modal with Test and Styling

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,11 @@
 import "Map.css";
-
+import AirportModal from "../AirportModal/AirportModal"
 
 export default function Map() {
     return (
+        <>
         <img src="Rate-My-Airport/src/assets/Blank_US_Map_(states_only).svg" alt="Map of the United States"/>
-
+        <AirportModal/>
+        </>
     );
 }
