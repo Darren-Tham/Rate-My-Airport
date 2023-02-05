3719123c76f81115849611a0e6a15463b0ce6599
Darren Tham <darrent9859@gmail.com>
2023-02-04 19:36:32 -0500

    Add dots to map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,13 +2,13 @@ import './Map.css';
 import AirportModal from './AirportModal/AirportModal'
 
 export default function Map() {
-    return (
-        <div className='parent'>
-        <img src="src/assets/Map.svg" alt="Map of the United States"/>
-        <AirportModal rating={"4/5"} acro={"ATL"} airportName={"Hartsfield"}/>
-        <span className="dot" id='jfk'></span>   
-        <span className="dot" id='lax'></span>
-        <span className="dot" id='atl'></span>
-        </div>
-    );
+  return (
+    <div className='parent'>
+      <img src="src/assets/Map.svg" alt="Map of the United States"/>
+      <AirportModal rating={"4/5"} acro={"ATL"} airportName={"Hartsfield"}/>
+      <span className="dot" id='jfk'></span>   
+      <span className="dot" id='lax'></span>
+      <span className="dot" id='atl'></span>
+    </div>
+  )
 }
